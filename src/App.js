b27b08b0ef9a556b29a1bd6b1ed23a880c7380f2
Facundo Fernandez <ff96_@hotmail.com>
2023-01-13 21:40:27 -0300

    API consumption and show data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import { personService } from "./services/PersonService";
 import { Component } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+import "primereact/resources/themes/nova/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 export default class App extends Component {
   constructor() {
@@ -10,12 +16,20 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.personService.getAll().then((data) => {
-      console.log(data);
-    });
+    this.personService
+      .getAll()
+      .then((data) => this.setState({ personas: data }));
   }
 
   render() {
-    return <h1>Hola mundo</h1>;
+    return (
+      <DataTable value={this.state.personas}>
+        <Column field="id" header="id"></Column>
+        <Column field="name" header="Name"></Column>
+        <Column field="lastname" header="lastname"></Column>
+        <Column field="address" header="address"></Column>
+        <Column field="cellphone" header="cellphone"></Column>
+      </DataTable>
+    );
   }
 }
--- src/services/PersonService.js
+++ src/services/PersonService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export class personService {
-  baseUrl = "http://localhost:8080/users/";
+  baseUrl = "http://localhost:8080/api/person/";
 
   getAll() {
-    return axios.get(this.baseUrl + "all").then((res) => res.data.data);
+    return axios.get(this.baseUrl + "all").then((res) => res.data);
   }
 }
