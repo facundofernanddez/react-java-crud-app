f8368d5a3a16c73f5c830008fe1025c97481543d
Facundo Fernandez <ff96_@hotmail.com>
2023-01-21 10:04:16 -0300

    Update SingUp.js

--- src/SingUp.js
+++ src/SingUp.js
@@ -1,15 +1,42 @@
 import { InputText } from "primereact/inputtext";
+import { useState } from "react";
 
-export const SingUp = () => {
-  return;
-  <>
-    <span className="p-float-label">
-      <InputText
-        id="in"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <label htmlFor="in">Username</label>
-    </span>
-  </>;
+export const SingUpForm = () => {
+  const [value, setValue] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  return (
+    <>
+      <span className="p-float-label">
+        <InputText
+          id="username"
+          value={value.username}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label htmlFor="username">Username</label>
+        <InputText
+          id="email"
+          value={value.email}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <InputText
+          id="password"
+          value={value.password}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <InputText
+          id="password2"
+          value={value.password2}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label htmlFor="password2">Repeat password</label>
+      </span>
+    </>
+  );
 };
