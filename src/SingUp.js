6266d5a5b695022664b35bbdff2dbe7ff6ed78ef
Facundo Fernandez <ff96_@hotmail.com>
2023-01-23 09:49:05 -0300

    Update SingUp.js

--- src/SingUp.js
+++ src/SingUp.js
@@ -9,34 +9,42 @@ export const SingUpForm = () => {
     password2: "",
   });
 
+  //arreglar bien este formulario de registro
+
   return (
     <>
-      <span className="p-float-label">
-        <InputText
-          id="username"
-          value={value.username}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <label htmlFor="username">Username</label>
-        <InputText
-          id="email"
-          value={value.email}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <label htmlFor="email">Email</label>
-        <InputText
-          id="password"
-          value={value.password}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <InputText
-          id="password2"
-          value={value.password2}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <label htmlFor="password2">Repeat password</label>
-      </span>
+      <form action="/logincheck">
+        <span className="p-float-label">
+          <InputText
+            id="username"
+            name="username"
+            value={value.username}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <label htmlFor="username">Username</label>
+          <InputText
+            id="email"
+            name="email"
+            value={value.email}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <InputText
+            id="password"
+            name="password"
+            value={value.password}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <InputText
+            id="password2"
+            name="password2"
+            value={value.password2}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <label htmlFor="password2">Repeat password</label>
+        </span>
+      </form>
     </>
   );
 };
